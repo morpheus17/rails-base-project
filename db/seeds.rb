66b0b3c5606c33a1eda5a0a63dabc2ece70ef79c
JC Ong <proxybetazero@gmail.com>
2021-04-24 12:03:37 +0800

    added base active admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -89,3 +89,4 @@ Transaction.new(
     buyer_id: User.find_by(username: "buyer").id
 ).save!
 
+AdminUser.create!(email: 'admin@example.com', password: '123456', password_confirmation: '123456') if Rails.env.development?
